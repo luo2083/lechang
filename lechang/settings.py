68125c3472c2d87bce44e27d38c571c4c2936bab
luo2083 <1354029556@qq.com>
2020-12-07 18:50:35 +0800

    modify settings.

--- lechang/settings.py
+++ lechang/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = evns.SECRET_KEY2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
